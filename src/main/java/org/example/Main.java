012357673cc643fd9425cb49594d98c5f1506947
tal6203 <112417918+tal6203@users.noreply.github.com>
2023-03-09 17:07:11 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,13 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
     public static void main(String[] args) {
-        AtomicReference<Logger> log1 = new AtomicReference<>();
-
-        AtomicReference<Logger> log2 = new AtomicReference<>();
-
-        Thread t1 = new Thread( () -> {
-            System.out.println(Thread.currentThread().getName());
-            log1.set(Logger.getInstance());
-        });
-        Thread t2 = new Thread(() -> {
-            System.out.println(Thread.currentThread().getName());
-            log2.set(Logger.getInstance());
-        });
-
-        t1.start();
-        t2.start();
+       Random rnd = new Random();
+        for(int i = 0; i < 8; i++) {
+            Thread t = new Thread( () -> {
+                Logger logger = Logger.getInstance();
+                logger.log_message(LogLevel.values()[rnd.nextInt(5)],"Message: massage");
+            });
+            t.start();
+        }
     }
 }
